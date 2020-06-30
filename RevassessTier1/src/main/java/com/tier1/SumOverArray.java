83dfd6c98d9e08484b3f37761287aa6ccf12ca1e
Steven Wilson <speedy0097@yahoo.com>
2020-06-30 11:25:52 -0400

    Uploading answers.

--- RevassessTier1/src/main/java/com/tier1/SumOverArray.java
+++ RevassessTier1/src/main/java/com/tier1/SumOverArray.java
@@ -3,6 +3,15 @@ package com.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		if(arr != null)
+		{
+			for(int i : arr)
+			{
+				sum = sum + i;
+			}
+		}
+		
+		return sum;
 	}
 }
